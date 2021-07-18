a837f891883c565989104d70b538d2929381eca3
kitutune <p.kitutune@gmail.com>
2021-07-19 00:54:26 +0900

    🔥 supabase/Uiのauthを使うことにしたので削除

--- components/Auth.js
+++ components/Auth.js
@@ -1,52 +1,31 @@
-import { useState } from 'react';
-import { supabase } from '../utils/supabaseClient';
+// import { Auth, Typography, Button } from '@supabase/ui';
+// import { createClient } from '@supabase/supabase-js';
 
-export const Auth = () => {
-  const [loading, setLoading] = useState(false);
-  const [email, setEmail] = useState('');
+// const supabase = createClient(
+//   'https://your-project-url.supabase.co',
+//   'PROJECT_ANON_KEY'
+// );
 
-  const handleLogin = async (email) => {
-    try {
-      setLoading(true);
-      const { error } = await supabase.auth.signIn({ email });
-      if (error) throw error;
-      alert('Check your email for the login link!');
-    } catch (error) {
-      alert(error.error_description || error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+// const Container = (props) => {
+//   const { user } = Auth.useUser();
+//   if (user)
+//     return (
+//       <>
+//         <Typography.Text>Signed in: {user.email}</Typography.Text>
+//         <Button block onClick={() => props.supabaseClient.auth.signOut()}>
+//           Sign out
+//         </Button>
+//       </>
+//     );
+//   return props.children;
+// };
 
-  return (
-    <div className="row flex flex-center">
-      <div className="col-6 form-widget">
-        <h1 className="header">Supabase + Next.js</h1>
-        <p className="description">
-          Sign in via magic link with your email below
-        </p>
-        <div>
-          <input
-            className="inputField"
-            type="email"
-            placeholder="Your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              handleLogin(email);
-            }}
-            className="button block"
-            disabled={loading}
-          >
-            <span>{loading ? 'Loading' : 'Send magic link'}</span>
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
+// export const AuthBasic = () => {
+//   return (
+//     <Auth.UserContextProvider supabaseClient={supabase}>
+//       <Container supabaseClient={supabase}>
+//         <Auth supabaseClient={supabase} />
+//       </Container>
+//     </Auth.UserContextProvider>
+//   );
+// };
