0fca41ef2a54323ba1ca3da5725bb1712283fb18
kitutune <p.kitutune@gmail.com>
2021-07-19 21:53:25 +0900

    💄 ChakuraUi追加

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import 'tailwindcss/tailwind.css';
-
+import { ChakraProvider } from '@chakra-ui/react';
+import { Auth } from '@supabase/ui';
+import { supabase } from 'utils/supabaseClient';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Auth.UserContextProvider supabaseClient={supabase}>
+        <Component {...pageProps} />
+      </Auth.UserContextProvider>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
