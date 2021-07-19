7c19b0582d909c9a4af33c3bd080d84b3e2f3278
kitutune <p.kitutune@gmail.com>
2021-07-20 06:08:01 +0900

    🚧 Profileコンポーネントを追加しlogoutメソッドをreturn外へ移動

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import { supabase } from 'utils/supabaseClient';
 import { Auth, Button } from '@supabase/ui';
+import { Profile } from 'components/Profile';
 
 export default function IndexPage() {
   const { user } = Auth.useUser();
+  const logout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) console.log('Error logging out:', error.message);
+  };
 
   return (
     <div className="w-full h-full bg-gray-300  ">
@@ -20,13 +25,8 @@ export default function IndexPage() {
           className="w-full h-full flex flex-col justify-center items-center p-4"
           style={{ minWidth: 250, maxWidth: 600, margin: 'auto' }}
         >
-          <Button
-            className="btn-black w-full mt-12"
-            onClick={async () => {
-              const { error } = await supabase.auth.signOut();
-              if (error) console.log('Error logging out:', error.message);
-            }}
-          >
+          <Profile logout={logout} uuid={user.id} />
+          <Button className="btn-black w-full mt-12" onClick={logout}>
             Logout
           </Button>
         </div>
