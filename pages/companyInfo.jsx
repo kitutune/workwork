42db767d9f932b7c1abd59b3bfe9eab7cf79b8d2
kitutune <p.kitutune@gmail.com>
2021-07-26 13:40:51 +0900

    🚧 見た目を変更

--- pages/companyInfo.jsx
+++ pages/companyInfo.jsx
@@ -8,88 +8,72 @@ import { supabase } from 'utils/supabaseClient';
 import { LayoutWrapper } from '../components/layoutWrapper';
 // import { SubtitleList } from '../components/subtitleList';
 
-const getCompanyInfo = async (id) => {
-  let { data, error } = await supabase
-    .from('企業情報')
-    .select('*')
-    .eq('id', id)
-    .single(); //data[0]ではなくdataで良くなる
-  // console.log(data);
-  if (!error && data) {
-    // const info = data[0];
-    const info = data;
-    ({ data, error } = await supabase
-      .from('企業情報補助')
-      .select('*')
-      //   .order('volume', { ascending: true }) //{小さい方から大き方へ:true}
-      .eq('企業情報', id)); //title_idはmanga_titleのid
-    console.log(info);
-    console.log(data);
-    if (!error && data) {
-      // console.log(data);
-      return { 会社名: data.会社名 || null, 最寄駅: data.最寄駅 };
-    } else {
-      return { 会社名: data.会社名 || null, 最寄駅: null };
-    }
-  }
-  return { 会社名: null, 最寄駅: null };
-};
-
 const companyInfo = () => {
   const Container = () => {
     const { user } = Auth.useUser();
-
-    const [subtitles, setSubtitles] = useState([]);
-    const [title, setTitle] = useState();
-
+    const [companyProf, setCompanyProf] = useState({});
     const router = useRouter();
-    // console.log(router);
     let { id } = router.query;
-    // console.log(router.query);
-    const getCompanyList = useCallback(async () => {
-      if (id) {
-        const { title, subtitles } = await getCompanyInfo(id.toString());
-        // console.log(title);
-        // console.log(subtitles);
-        if (title) {
-          setTitle(title);
-        }
-        // else {
-        //   router.push('/');
-        // }
-        // ここから　追加したファイル表示（①がないと読みこれまない）
-        if (subtitles) {
-          setSubtitles(subtitles);
-        }
-        // ここまで　追加したファイル表示（①がないと読みこれまない）
-      }
-    }, [id, router]);
-    // ここから　ファイルの表示
-    useEffect(() => {
-      if (!id) {
-        router.push('/');
-      }
-      getCompanyList();
-    }, [
-      user,
-      //  getCompanyList,
-      id,
-      router,
-    ]);
-    // ここまで　ファイルの表示①
+    const companyName = companyProf.会社名;
+    console.log(companyProf);
+    console.log('companyProfのログ');
+    console.log(companyName);
+    // console.log(user);
+    // console.log('companyinfoのuser');
+
+    // console.log(router);
+
+    // console.log(id);
+    // console.log('ここまで');
+
+    // -----------------A
+    // const sample = async () => {
+    //   let { data, error } = await supabase
+    //     .from('企業情報')
+    //     .select('*')
+    //     .eq('id', id);
+    //   // .single();
+    //   // console.log(data);
+    //   return data;
+    // };
+
+    // const data = useCallback(async () => await sample(), []);
+    // console.log(sample());
+    // console.log('別バージョンのsample');
+    // console.log(sample);
+    // -------------------AA
+    // ------------------B
+    useEffect(async () => {
+      console.log('マウントされた時');
+      const { data, error } = await supabase
+        .from('企業情報')
+        .select('*')
+        .eq('id', id)
+        .single();
+      // console.log(data);
+      setCompanyProf(data);
+      // setCompanyProf(data[0] || {});
+      // console.log(setCompanyProf(data));
+      return () => {
+        console.log('アンマウントされた時');
+      };
+    }, []);
+
+    // ----------------------BB
     if (user) {
       return (
         <div>
+          {/* {console.log(sample())} */}
+          {/* {console.log({ data })} */}
+          {/* {console.log(user)}
+          {console.log('companyinfoのuser')}
+         
+          {console.log('sampleの中身')} */}
+          {/* {console.log(data.最寄駅)} */}
+          {/* {console.log('companyinfoのinfo　次にdata')} */}
           <div className="flex justify-end gap-2 my-2 mr-2">
-            {' '}
             {/* 右端寄席のCSS */}
-            {/* Editボタン表示　ここから */}
-            {title && (
-              <div className="w-24">
-                {/* <EditTitle title={title} getCompanyList={getCompanyList} /> */}
-              </div>
-            )}
-            {/* Editボタン表示　ここまで */}
+
             {/* Backボタン表示　ここから */}
             <div className="w-24">
               <Back>
@@ -100,18 +84,6 @@ const companyInfo = () => {
             </div>
             {/* Backボタン表示　ここまで */}
           </div>
-          {/* このページフォルダのタイトル表示　ここから */}
-          {/* {title && (
-            <>
-              <h2 className="pb-4 text-4xl font-bold text-center">
-                {title.title}
-              </h2>
-              <p className="pb-4 text-2xl font-semibold text-center">
-                {title.author}
-              </p>
-            </>
-          )} */}
-          {/* このページフォルダのタイトル表示　ここまで */}
 
           <h1>aaaaa</h1>
           {/* ここから追加 */}
@@ -133,45 +105,88 @@ const companyInfo = () => {
                   {/* <!-- Col --> */}
                   <div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
                     <h3 className="pt-4 text-2xl text-center">
-                      Create an Account!
+                      {/* Create an Account! */}
+                      {companyProf.会社名}
                     </h3>
                     <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded">
-                      <div className="mb-4 md:flex md:justify-between">
-                        <div className="mb-4 md:mr-2 md:mb-0">
-                          <label
-                            className="block mb-2 text-sm font-bold text-gray-700"
-                            for="firstName"
-                          >
-                            First Name
-                          </label>
-                          <input
-                            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                            id="firstName"
-                            type="text"
-                            placeholder="First Name"
-                          />
-                        </div>
-                        <div className="md:ml-2">
-                          <label
-                            className="block mb-2 text-sm font-bold text-gray-700"
-                            for="lastName"
-                          >
-                            Last Name
-                          </label>
-                          <input
-                            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                            id="lastName"
-                            type="text"
-                            placeholder="Last Name"
-                          />
+                      {/* 　項目ここから */}
+                      <div className="mb-4　">
+                        <label className="block mb-2 text-sm　 font-bold text-gray-700">
+                          住所
+                        </label>
+                        <div
+                          className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                          id="email"
+                          type="email"
+                        >
+                          {companyProf.住所}
                         </div>
                       </div>
-                      <div className="mb-4">
+                      {/* 　項目ここまで */}
+                      {/* 　項目ここから */}
+                      <div className="mb-4　">
+                        <label className="block mb-2 text-sm　 font-bold text-gray-700">
+                          電話番号
+                        </label>
+                        <div
+                          className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                          id="email"
+                          type="email"
+                        >
+                          {companyProf.電話番号}
+                        </div>
+                      </div>
+                      {/* 　項目ここまで */}
+                      {/* 　項目ここから */}
+                      <div className="mb-4　">
+                        <label className="block mb-2 text-sm　 font-bold text-gray-700">
+                          資本金
+                        </label>
+                        <div
+                          className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                          id="email"
+                          type="email"
+                        >
+                          {companyProf.資本金}
+                        </div>
+                      </div>
+                      {/* 　項目ここまで */}
+                      {/* 　項目ここから */}
+                      <div className="mb-4　">
+                        <label className="block mb-2 text-sm　 font-bold text-gray-700">
+                          社員数
+                        </label>
+                        <div
+                          className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                          id="email"
+                          type="email"
+                        >
+                          {companyProf.社員数}
+                        </div>
+                      </div>
+                      {/* 　項目ここまで */}
+                      {/* 　項目ここから */}
+                      <div className="mb-4　">
+                        <label className="block mb-2 text-sm　 font-bold text-gray-700">
+                          設立日
+                        </label>
+                        <div
+                          className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                          id="email"
+                          type="email"
+                        >
+                          {companyProf.設立日}
+                        </div>
+                      </div>
+                      {/* 　項目ここまで */}
+
+                      {/* 長い奴　ここから */}
+                      {/* <div className="mb-4">
                         <label
                           className="block mb-2 text-sm font-bold text-gray-700"
-                          for="email"
+                          // for="email"
                         >
-                          Email
+                          Email{companyProf.電話番号}
                         </label>
                         <input
                           className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
@@ -179,46 +194,16 @@ const companyInfo = () => {
                           type="email"
                           placeholder="Email"
                         />
-                      </div>
-                      <div className="mb-4 md:flex md:justify-between">
-                        <div className="mb-4 md:mr-2 md:mb-0">
-                          <label
-                            className="block mb-2 text-sm font-bold text-gray-700"
-                            for="password"
-                          >
-                            Password
-                          </label>
-                          <input
-                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                            id="password"
-                            type="password"
-                            placeholder="******************"
-                          />
-                          <p className="text-xs italic text-red-500">
-                            Please choose a password.
-                          </p>
-                        </div>
-                        <div className="md:ml-2">
-                          <label
-                            className="block mb-2 text-sm font-bold text-gray-700"
-                            for="c_password"
-                          >
-                            Confirm Password
-                          </label>
-                          <input
-                            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                            id="c_password"
-                            type="password"
-                            placeholder="******************"
-                          />
-                        </div>
-                      </div>
+                      </div> */}
+                      {/* 長い奴　ここまで */}
+
                       <div className="mb-6 text-center">
                         <button
                           className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
                           type="button"
                         >
                           Register Account
+                          {/* {companyProf.地図} */}
                         </button>
                       </div>
                       <hr className="mb-6 border-t" />
@@ -254,7 +239,9 @@ const companyInfo = () => {
   return (
     <LayoutWrapper>
       <Auth.UserContextProvider supabaseClient={supabase}>
-        <Container />
+        <Container
+        //  id={id}
+        />
       </Auth.UserContextProvider>
     </LayoutWrapper>
   );
