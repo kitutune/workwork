51d6ea9e50cdc2d58cb4d97c33f6459a325c0d60
kitutune <p.kitutune@gmail.com>
2021-07-26 20:58:51 +0900

    🚧 ボタンでメニューのオンオフができる機能追加（スタイルはまだ未形成）

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,87 +2,102 @@ import { Back } from './back';
 import { Profile } from './Profile';
 import { ToolModal } from './ToolModal';
 import { Auth } from '@supabase/ui';
+import { useState } from 'react';
 export const Header = () => {
   const { user } = Auth.useUser();
-  console.log(user);
-  console.log('かかってるよ！');
+  // console.log(user);
+  // console.log('headerのuser');
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen((prevState) => !prevState);
+  };
   return (
-    <header className="text-gray-600 bg-gray-200">
-      <nav className="flex items-center justify-between flex-wrap bg-teal p-6">
-        <div className="flex items-center flex-no-shrink text-white mr-6">
-          <Back>
-            <svg
-              className="h-8 w-8 mr-2"
-              width="54"
-              height="54"
-              viewBox="0 0 54 54"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-            </svg>
-            <span className="font-semibold text-xl tracking-tight">
-              Tailwind CSS
-            </span>
-          </Back>
-        </div>
-        <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
-            <svg
-              className="h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div className="text-sm lg:flex-grow">
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4"
-            >
-              Docs
-            </a>
-
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4"
-            >
-              Examples
-            </a>
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white"
-            >
-              Blog
-            </a>
+    <>
+      <header className="text-gray-600 bg-gray-200">
+        <nav className="flex items-center justify-between flex-wrap bg-teal p-6">
+          <div className="flex items-center flex-no-shrink text-white mr-6">
+            <Back>
+              <svg
+                className="h-8 w-8 mr-2"
+                width="54"
+                height="54"
+                viewBox="0 0 54 54"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+              </svg>
+              <span className="font-semibold text-xl tracking-tight">
+                Tailwind CSS
+              </span>
+            </Back>
           </div>
-          <div>
-            <a
-              href="#"
-              className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0"
-            >
-              Download
-            </a>
+          <h1>ccccc</h1>
 
-            {!user ? (
-              <div></div>
+          {/* menu */}
+          <div className="block lg:hidden">
+            <button
+              onClick={toggle}
+              className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white"
+            >
+              <svg
+                className="h-3 w-3"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+              </svg>
+            </button>
+          </div>
+          <h1>bbbbb</h1>
+          {/* menu */}
+
+          <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            {open ? (
+              <div className="text-sm lg:flex-grow">
+                <a
+                  href="#responsive-header"
+                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4"
+                >
+                  Docs
+                </a>
+
+                <a
+                  href="#responsive-header"
+                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4"
+                >
+                  Examples
+                </a>
+                <a
+                  href="#responsive-header"
+                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white"
+                >
+                  Blog
+                </a>
+              </div>
             ) : (
-              <ToolModal>
-                <Profile
-                  // button1osita={() => {
-                  //   alert('cancel');
-                  // }}
-                  // button1="sugoi"
-                  uuid={user.id}
-                />
-              </ToolModal>
+              <div></div>
             )}
+
+            <div>
+              {!user ? (
+                <div></div>
+              ) : (
+                <ToolModal>
+                  <Profile
+                    // button1osita={() => {
+                    //   alert('cancel');
+                    // }}
+                    // button1="sugoi"
+                    uuid={user.id}
+                  />
+                </ToolModal>
+              )}
+            </div>
           </div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+    </>
   );
 };
