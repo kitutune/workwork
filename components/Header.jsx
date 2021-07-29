effe18ebc3b284334a41ea230e291312e00e4148
kitutune <p.kitutune@gmail.com>
2021-07-30 08:46:43 +0900

    🚧 アイコン変更とimageコンポーネントの利用

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,7 @@ import { Profile } from './Profile';
 import { ToolModal } from './ToolModal';
 import { Auth } from '@supabase/ui';
 import { useState } from 'react';
+import Image from 'next/image';
 import cc from 'classcat';
 export const Header = () => {
   const { user } = Auth.useUser();
@@ -19,7 +20,7 @@ export const Header = () => {
         <nav className="flex items-center justify-between flex-wrap bg-teal p-6">
           <div className="flex items-center flex-no-shrink text-white mr-6">
             <Back>
-              <svg
+              {/* <svg
                 className="h-8 w-8 mr-2"
                 width="54"
                 height="54"
@@ -30,85 +31,80 @@ export const Header = () => {
               </svg>
               <span className="font-semibold text-xl tracking-tight">
                 Tailwind CSS
-              </span>
+              </span> */}
+              <Image
+                width={54}
+                height={54}
+                // layout={'intrinsic'}
+                alt="humanicon"
+                src="/human.png"
+                viewBox="0 0 54 54"
+                // className="z-1"
+              />
             </Back>
           </div>
-
-          {/* menu */}
-          <div className="block lg:hidden">
-            <button
-              onClick={toggle}
-              className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white"
-            >
-              <svg
-                className="h-3 w-3"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>Menu</title>
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
-            </button>
-          </div>
-
-          {/* menu */}
           {!user ? (
             <div></div>
           ) : (
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-              {/* {open ? <div className={cc([
-                      'text-sm lg:flex-grow md:block',
-                      {
-                        'hidden': !open,
-                       
-                      },
-                    ])}></div> : <div></div>} */}
-
-              {/* <div className="text-sm lg:flex-grow md:block"> */}
-              <div
-                className={cc([
-                  'text-sm lg:flex-grow  md:block',
-                  {
-                    hidden: !open,
-                  },
-                ])}
-              >
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4"
+            <>
+              <div className="block lg:hidden">
+                <button
+                  onClick={toggle}
+                  className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white"
                 >
-                  Docs
-                </a>
-
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4"
-                >
-                  Examples
-                </a>
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white"
-                >
-                  Blog
-                </a>
+                  <svg
+                    className="h-3 w-3"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Menu</title>
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                  </svg>
+                </button>
               </div>
-              <div>
-                {/* {!user ? (
-                <div></div>
-              ) : ( */}
-                <ToolModal>
-                  <Profile
-                  // button1osita={() => {
-                  //   alert('cancel');
-                  // }}
-                  // button1="sugoi"
-                  // uuid={user.id}
-                  />
-                </ToolModal>
-                {/* // )} */}
+
+              <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <div
+                  className={cc([
+                    'text-sm lg:flex-grow  md:block',
+                    {
+                      hidden: !open,
+                    },
+                  ])}
+                >
+                  <a
+                    href="#responsive-header"
+                    className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4"
+                  >
+                    Docs
+                  </a>
+
+                  <a
+                    href="#responsive-header"
+                    className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4"
+                  >
+                    Examples
+                  </a>
+                  <a
+                    href="#responsive-header"
+                    className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white"
+                  >
+                    Blog
+                  </a>
+                </div>
+                <div>
+                  <ToolModal>
+                    <Profile
+                      // button1osita={() => {
+                      //   alert('cancel');
+                      // }}
+                      // button1="sugoi"
+                      uuid={user.id}
+                    />
+                  </ToolModal>
+                </div>
               </div>
-            </div>
+            </>
           )}
         </nav>
       </header>
