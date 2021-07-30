4823c9c98980cf8abdab6761f6500fd0b63b4c9e
kitutune <p.kitutune@gmail.com>
2021-07-30 10:10:17 +0900

    🚧 ハンバーガーメニューのオンオフでアイコンが切り替わるように設定

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ export const Header = () => {
   console.log(user);
   console.log('headerのuser');
   const [open, setOpen] = useState(false);
-  console.log(open);
+  // console.log(open);
   const toggle = () => {
     setOpen((prevState) => !prevState);
   };
@@ -27,19 +27,36 @@ export const Header = () => {
             <div></div>
           ) : (
             <>
+              {console.log(open)}
+              {console.log('open')}
               <div className="block lg:hidden">
                 <button
                   onClick={toggle}
                   className="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white"
                 >
-                  <svg
+                  {open ? (
+                    <Image
+                      width={20}
+                      height={20}
+                      alt="menuicon"
+                      src="/menu.png"
+                    />
+                  ) : (
+                    <Image
+                      width={20}
+                      height={20}
+                      alt="batuicon"
+                      src="/batu.png"
+                    />
+                  )}
+                  {/* <svg
                     className="h-3 w-3"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <title>Menu</title>
                     <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                  </svg>
+                  </svg> */}
                 </button>
               </div>
 
@@ -83,20 +100,6 @@ export const Header = () => {
                   >
                     Blog
                   </a>
-                </div>
-                <div className={cc(['p-4', { hidden: !open }])}>
-                  <ToolModal
-                    style="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white"
-                    name="Profile"
-                  >
-                    <Profile
-                      // button1osita={() => {
-                      //   alert('cancel');
-                      // }}
-                      // button1="sugoi"
-                      uuid={user.id}
-                    />
-                  </ToolModal>
                 </div>
               </div>
             </>
