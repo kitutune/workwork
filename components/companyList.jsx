e127aae712e839c8eab1c03411d725c073d76abf
kitutune <p.kitutune@gmail.com>
2021-08-01 14:49:19 +0900

    🚧 企業一覧ページで企業のサムネイルを表示するように変更

--- components/companyList.jsx
+++ components/companyList.jsx
@@ -1,32 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import companyIcon from 'public/company.png';
-// import React from 'react';
 import { Addcompany } from '../components/addCompany';
-// import { AddTitle } from "src/components/addTitle";
-
-// export type Title = {
-//   id: number;
-//   user_id: string;
-//   company: string;
-//   author: string;
-//   image_url: string;
-// };
-
-// type TitlesProps = {
-//   companys: Title[];
-//   uuid: string;
-//   getCompanyList: VoidFunction;
-//   filterText: string;
-// };
 
 export const CompanyList = (props) => {
   const filteredTitle = props.companyNames.filter((company) => {
     let searchContent = company['会社名'] + ' ' + company.URL;
     return searchContent.toLowerCase().includes(props.filterText.toLowerCase());
   });
-  console.log(props.uuid);
-  console.log('companyListのuuid');
+
   return (
     <div className="grid grid-cols-3 gap-2 m-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8">
       <Addcompany uuid={props.uuid} getCompanyList={props.getCompanyList} />
@@ -39,12 +21,22 @@ export const CompanyList = (props) => {
           >
             <div className="p-2 border cursor-pointer">
               <div className="flex justify-center">
-                <Image
-                  src={companyIcon}
-                  alt="thumbnail"
-                  width={126}
-                  height={200}
-                />
+                {company.求人URL ? (
+                  <div>
+                    <img
+                      src={`http://capture.heartrails.com/126x200/cool?${company.URL}`}
+                      width={126}
+                      height={200}
+                    />
+                  </div>
+                ) : (
+                  <Image
+                    src={companyIcon}
+                    alt="thumbnail"
+                    width={126}
+                    height={200}
+                  />
+                )}
               </div>
               <div className="mt-2 text-center">{company['会社名']}</div>
             </div>
