681a073de6ac61abbc42bfb34872a0385c8b2033
kitutune <p.kitutune@gmail.com>
2021-08-02 16:35:21 +0900

    🚧 モーダルの下にモーダルを閉じるボタンが表示されるのを修正

--- components/ToolModal.jsx
+++ components/ToolModal.jsx
@@ -65,32 +65,6 @@ export const ToolModal = (props) => {
               </div>
               &#8203;
             </span>
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              {/* <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl"> */}
-
-              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-                {/* <div> */}
-                {/* ToolModalで挟んだ中が反映される */}
-
-                <div className="mt-4">
-                  <button
-                    type="button"
-                    className="inline-flex  justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                    onClick={closeModal}
-                  >
-                    back
-                  </button>
-                </div>
-              </div>
-            </Transition.Child>
           </div>
         </Dialog>
       </Transition>
