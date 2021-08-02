cd66bde6d0884b2c428817ca8c3bccda3727147b
kitutune <p.kitutune@gmail.com>
2021-08-02 22:01:55 +0900

    🚧 作成途中だがより条件に合うMAP表示が見つかったので保留する

--- components/useMapGeocode.jsx
+++ components/useMapGeocode.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useCallback, useEffect, useState } from 'react';
 // import { Map } from './map';
 
-export const useMapGeocode = (address) => {
+export const useMapGeocode = (props) => {
   const [lat, setLat] = useState(null);
   const [lon, setLon] = useState(null);
   return useCallback(() => {
@@ -13,37 +13,40 @@ export const useMapGeocode = (address) => {
     // console.log('この上２個');
     useEffect(() => {
       // const response = await axios.get(`/api/yahooGeocode?address=${address}`);
-      // console.log('ここまで来れている？');
+      console.log(props);
+      console.log('ここまで来れている？');
 
       if (address) {
-        axios.get(`/api/yahooGeocode?address=${address}`).then((response) => {
-          console.log(response.data.Feature[0].Geometry.Coordinates);
+        axios
+          .get(`/api/yahooGeocode?address=${props.address}`)
+          .then((response) => {
+            console.log(response.data.Feature[0].Geometry.Coordinates);
 
-          // const geo = response.data.Feature[0].Geometry.Coordinates.split(',');
-          if (response) {
-            const geo =
-              response.data.Feature[0].Geometry.Coordinates.split(',');
-            // console.log(geo);
-            // console.log('ここ');
-            // console.log(geo[0]);
-            if (geo[0] && geo[1]) {
-              setLat(geo[1]);
-              setLon(geo[0]);
+            // const geo = response.data.Feature[0].Geometry.Coordinates.split(',');
+            if (response) {
+              const geo =
+                response.data.Feature[0].Geometry.Coordinates.split(',');
+              // console.log(geo);
+              // console.log('ここ');
+              // console.log(geo[0]);
+              if (geo[0] && geo[1]) {
+                setLat(geo[1]);
+                setLon(geo[0]);
+              }
+              // console.log(lat);
+              // console.log(lon);
+              // console.log('この上２個');
             }
-            // console.log(lat);
-            // console.log(lon);
-            // console.log('この上２個');
-          }
-        });
+          });
       }
 
       return;
-    }, [address]);
+    }, []);
     console.log({ lat, lon });
     console.log('手前の奴です');
 
     return { lat: Number(lat), lng: Number(lon) };
-  }, [address, lat, lon])();
+  }, [lat, lon])();
 };
 // console.log(axios);
 
