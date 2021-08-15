10dce05edba43ba5a41294dbc0feb6d12516d6f7
kitutune <p.kitutune@gmail.com>
2021-08-15 22:27:22 +0900

    🚧 修正中

--- components/companyList.jsx
+++ components/companyList.jsx
@@ -1,10 +1,51 @@
+//ここっから
 import Image from 'next/image';
 import Link from 'next/link';
 import companyIcon from 'public/company.png';
+import { useEffect, useState } from 'react';
 import { Addcompany } from './addCompany';
 // import { Addcompany } from './AddCompany';
 
 export const CompanyList = (props) => {
+  // const [searchCompany, setSearchCompany] = useState({});
+  // const [loading, setLoading] = useState(true);
+  // console.log(props);
+  // console.log('11');
+  // useEffect(() => {
+  //   async () => {
+  //     if (props.allData !== undefined) {
+  //       const searchCompany = await props.allData.filter((company) => {
+  //         let searchContent =
+  //           company['company_name'] +
+  //           ' ' +
+  //           company['URL'] +
+  //           ' ' +
+  //           company['access'] +
+  //           ' ' +
+  //           company['capital_stock'] +
+  //           ' ' +
+  //           company['company_address'] +
+  //           ' ' +
+  //           company['work'];
+  //         return setSearchCompany(
+  //           searchContent.toLowerCase().includes(props.filterText.toLowerCase())
+  //         );
+  //       });
+  //       // if (searchCompany) {
+  //       //   setLoading(false);
+  //       //   console.log('setLoading(false);');
+  //       // }
+  //     } else {
+  //       props.getCompanyList();
+  //       console.log(' props.getCompanyList();');
+  //     }
+  //   };
+
+  //   return () => {
+  //     // cleanup;
+  //   };
+  // }, []);
+
   const searchCompany = props.allData.filter((company) => {
     let searchContent =
       company['company_name'] +
@@ -21,42 +62,55 @@ export const CompanyList = (props) => {
     return searchContent.toLowerCase().includes(props.filterText.toLowerCase());
   });
 
-  return (
-    <div className="grid grid-cols-3 gap-2 m-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8">
-      <Addcompany uuid={props.uuid} getCompanyList={props.getCompanyList} />
-      {searchCompany.map((company) => {
-        return (
-          <Link
-            key={company.company_id}
-            href={`/companyInfo?id=${company.company_id}`}
-            passHref
-            userId={props.uuid}
-          >
-            <div className="p-2 border cursor-pointer">
-              <div className="flex justify-center">
-                {company.URL ? (
-                  <div>
+  console.log(props.allData);
+  console.log('props.allData');
+
+  console.log(searchCompany.length);
+  console.log('searchCompany');
+  if (searchCompany.length !== undefined) {
+    return (
+      <div className="grid grid-cols-3 gap-2 m-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8">
+        {/* {console.log(searchCompany)}
+      {console.log('外外外')} */}
+        <Addcompany uuid={props.uuid} getCompanyList={props.getCompanyList} />
+        {searchCompany.map((company) => {
+          return (
+            <Link
+              key={company.company_id}
+              href={`/companyInfo?id=${company.company_id}`}
+              passHref
+              userId={props.uuid}
+            >
+              <div className="p-2 border cursor-pointer">
+                <div className="flex justify-center">
+                  {company.URL ? (
+                    <div>
+                      <Image
+                        src={`http://capture.heartrails.com/126x200/cool?${company.URL}`}
+                        width={126}
+                        height={200}
+                        alt="CompanyThumbnail"
+                      />
+                    </div>
+                  ) : (
                     <Image
-                      src={`http://capture.heartrails.com/126x200/cool?${company.URL}`}
+                      src={companyIcon}
+                      alt="NoThumbnail"
                       width={126}
                       height={200}
-                      alt="CompanyThumbnail"
                     />
-                  </div>
-                ) : (
-                  <Image
-                    src={companyIcon}
-                    alt="NoThumbnail"
-                    width={126}
-                    height={200}
-                  />
-                )}
+                  )}
+                </div>
+                <div className="mt-2 text-center">
+                  {company['company_name']}
+                </div>
               </div>
-              <div className="mt-2 text-center">{company['company_name']}</div>
-            </div>
-          </Link>
-        );
-      })}
-    </div>
-  );
+            </Link>
+          );
+        })}
+      </div>
+    );
+  }
+
+  return <>{props.children}</>;
 };
--- pages/index.js
+++ pages/index.js
@@ -1,75 +1,72 @@
+//　更新します
 import { Auth, Button, IconLogOut } from '@supabase/ui';
 import React, { useCallback, useEffect, useState } from 'react';
 import { supabase } from 'utils/supabaseClient';
-// import { CompanyList } from 'components/CompanyList';
 import { FcLikePlaceholder, FcLike } from 'react-icons/fc';
 import { LayoutWrapper } from 'components/LayoutWrapper';
 import { CompanyList } from 'components/companyList';
-import { Addcompany } from 'components/addCompany';
-// const getCompany = async () => {
-//   const { data, error } = await supabase
-//     .from('company')
-//     .select('*')
-//     .order('company_name');
-//   if (!error && data) {
-//     return data;
-//   }
-//   return [];
-// };
+
+const getBookmarkWithDb = async () => {
+  console.log('22');
+  const { data, error } = await supabase
+    .from('company_info_flug')
+    .select('*')
+    .eq('user_id', user.id)
+    .eq('bookmark', true);
+  return data;
+};
+
+const getDb = async () => {
+  console.log('44');
+  const { data, error } = await supabase.from('company_info_flug').select('*');
+  return data;
+};
 
 const Container = (props) => {
   const { user } = Auth.useUser();
-  const [allData, setAllData] = useState({});
+
+  const [allData, setAllData] = useState();
   const [bookmark, setBookmark] = useState(false);
   const [text, setText] = useState('');
-  // const [companyNames, setCompanyNames] = useState([]);
 
-  // const getCompanyList = useCallback(async () => {
-  //   const data = await getCompany();
-  //   setCompanyNames(data);
-  // }, []);
-  const bookmarkButton = useCallback(async () => {
+  const bookmarkButton = () => {
     setBookmark((bookmark) => !bookmark);
-  }, []);
-  // useEffect(() => {
-  //   getCompanyList();
-  // }, [user, getCompanyList]);
-
-  const getCompanyList = useCallback(() => {
+  };
+  console.log(bookmark);
+  const getCompanyList = useCallback(async () => {
+    console.log('getCompanyList2');
+    console.log(user);
+    console.log(bookmark);
+    console.log('getCompanyList1');
     if (user) {
       if (bookmark) {
-        supabase
-          .from('company_info_flug')
-          .select('*')
-          .eq('user_id', user.id)
-          .eq('bookmark', true)
-          .then((db) => {
-            setAllData(db.data);
-            console.log('読み込みましたtrue');
-          });
+        const data = await getBookmarkWithDb();
+        setAllData(data);
       } else {
-        supabase
-          .from('company_info_flug')
-          .select('*')
-          .then((db) => {
-            setAllData(db.data);
-            console.log('読み込みましたfalse');
-          });
+        const data = await getDb();
+        setAllData(data);
       }
     }
-  }, [user, bookmark]);
-  useEffect(() => {
-    if (user) {
-      getCompanyList(user, bookmark);
-      // console.log(bookmark);
-      // console.log(allData);
-      // console.log('ここには来てる');
-    }
-  }, [user, bookmark, getCompanyList, allData]);
+  }, [user]);
 
+  useEffect(async () => {
+    await getCompanyList();
+  }, [user, getCompanyList, bookmark]);
+
+  useEffect(() => {
+    let unmounted = false;
+    //クリーンアップ関数を返す
+    return () => {
+      unmounted = true;
+    };
+  });
+
+  console.log('index');
   if (user && allData) {
     return (
       <div>
+        {console.log(allData)}
+        {console.log('indexのallData')}
         <div className="flex justify-center gap-2 p-4">
           <input
             className="w-full h-12 px-4 bg-white border border-gray-300 rounded shadow appearance-none hover:border-gray-700"
@@ -82,18 +79,14 @@ const Container = (props) => {
             {bookmark ? <FcLike /> : <FcLikePlaceholder />}
           </button>
         </div>
-        {allData.length === undefined ? (
-          <div>loading...</div>
-        ) : (
-          <CompanyList
-            allData={allData}
-            // companyNames={companyNames}
-            uuid={user.id}
-            getCompanyList={getCompanyList}
-            filterText={text}
-          />
-        )}
-
+        {/* {loading ? <div>loading...</div> : <>aa</>} */}
+        <CompanyList
+          allData={allData}
+          // companyNames={companyNames}
+          uuid={user.id}
+          getCompanyList={getCompanyList}
+          filterText={text}
+        />
         <div className="flex justify-end mx-2 my-4">
           <Button
             size="medium"
