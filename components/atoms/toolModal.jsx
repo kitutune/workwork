325e8b7c03f82b89d871e488bb91fb07db267854
kitutune <p.kitutune@gmail.com>
2021-08-17 14:10:01 +0900

    🚚 atomsフォルダを作成して移動

--- components/atoms/toolModal.jsx
+++ components/atoms/toolModal.jsx
@@ -45,11 +45,7 @@ export const ToolModal = (props) => {
                 {props.children}
               </div>
               <div className="absolute right-0 z-20 top-0 p-4">
-                <button
-                  type="button"
-                  className=" justify-center px-4 py-2 text-sm font-medium  font-bold text-whitebg-transparent border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                  onClick={closeModal}
-                >
+                <button type="button" onClick={closeModal}>
                   <Image
                     width={54}
                     height={54}
